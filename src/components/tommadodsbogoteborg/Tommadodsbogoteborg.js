887c6a5875c4de33c0da02192840bcb6c867cb0d
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-12-22 13:29:40 +0100

    test

--- src/components/tommadodsbogoteborg/Tommadodsbogoteborg.js
+++ src/components/tommadodsbogoteborg/Tommadodsbogoteborg.js
@@ -14,6 +14,7 @@ import LottieBooking from "../../LottieAnimation/LottieBooking";
 import booking from "../../utils/animation/booking.json";
 import BookingModal from "../BookingModal";
 const Tommadodsbogoteborg = ({ videoText }) => {
+  console.log('test')
   const [modalOpen, setModalOpen] = useState(false);
   const newsData = useSelector(getNewsData);
   const [content, setContent] = useState({
